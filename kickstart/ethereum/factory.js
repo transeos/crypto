55525e4fbbea9e1fcabdad7f9dcd455d79a5ff7d
Hiranmoy Basak <transeos>
2021-11-28 00:33:45 +0530

    updating campaign factory address

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   CampaignFactory.abi,
-  '0xD32920bb10A1FecB922FD67Ae114669aA4E865d3'
+  '0xCF6D6fb562F57e3FF226962FF6628f06cD9929fC'
 );
 
 export default instance;
